7819f716902b1391a15283c3dbea4d2c76af7e9f
David Ebbo <david.ebbo@gmail.com>
2023-03-26 09:25:01 -0700

    Put all vernacular lines at the end

--- oz_tree_build/utilities/trim_wikipedia.py
+++ oz_tree_build/utilities/trim_wikipedia.py
@@ -7,59 +7,53 @@ import argparse
 import bz2
 import json
 import logging
-import re
 import sys
 import time
 
-match_taxa = {
-    16521: 'taxon',
-    310890: 'monotypic taxon',
-    23038290: 'fossil taxon',
-    713623: 'clade',
-}
-match_vernacular = {
-    502895: 'common name',
-    55983715: 'group of organisms known by one particular common name',
-}
-
-regexp_match = '|'.join([str(v) for v in list(match_taxa) + list(match_vernacular)])
-quick_byte_match = re.compile('numeric-id":(?:{})\D'.format(regexp_match))
-
+from oz_tree_build.utilities.temp_helpers import *
 
 known_claims = { "P31", "P685", "P846", "P850", "P1391", "P5055", "P830", "P141", "P627", "P961" }
 
-def enumerate_lines_from_bz2_file(filename):
-    with bz2.open(filename, 'rt') as f:
+def enumerate_lines_from_file(filename):
+    if filename.endswith('.bz2'):
+        f = bz2.open(filename, 'rt')
+    else:
+        f = open(filename, 'rt')
+    with f:
         for line_num, line in enumerate(f):
             yield line_num, line
 
-def enumerate_lines_from_plain_file(filename):
-    with open(filename, 'rt') as f:
-        for line_num, line in enumerate(f):
-            yield line_num, line
+def process_all_lines(wikipedia_file, output_stream, language='en'):
 
-def process_all_lines(line_enumerator, output_stream, language='en'):
     output_stream.write('[\n')
-    first = True
     preserved_lines = 0
+
+    # We want all the vernacular lines to end up at the end of the file, so we
+    # store them in a list. There are only a few hundred, so memory isn't
+    # an issue.
+    vernacular_lines = []
 
     sitelinks_key = f'{language}wiki'
 
-    for line_num, line in line_enumerator:
+    for line_num, line in enumerate_lines_from_file(wikipedia_file):
         if (line_num > 0 and line_num % 100000 == 0):
-            logging.info(f"Kept {preserved_lines} out of {line_num} processed lines ({preserved_lines / line_num * 100:.2f}%))")
+            logging.info(f"Kept {preserved_lines} out of {line_num-1} processed lines ({preserved_lines / line_num * 100:.2f}%))")
 
-        # if not(line.startswith('{"type":') and quick_byte_match.search(line)):
-        if not line.startswith('{"type":'):
+        if not(line.startswith('{"type":') and quick_byte_match.search(line)):
+            continue
+
+        json_item = json.loads(line.rstrip().rstrip(","))
+
+        try:
+            is_taxon, vernaculars = find_taxon_and_vernaculars(json_item)
+        except KeyError:
+            continue
+
+        # If it's neither, ignore it
+        if not is_taxon and not len(vernaculars) > 0:
             continue
 
         preserved_lines += 1
-
-        if not first:
-            output_stream.write(',\n')
-        first = False
-
-        json_item = json.loads(line.rstrip().rstrip(","))
 
         # Remove things we don't need at all
         if 'descriptions' in json_item:
@@ -81,17 +75,31 @@ def process_all_lines(line_enumerator, output_stream, language='en'):
         # rest is just needed to collect the language names into the bit field
         json_item['sitelinks'] = {k: v if k==sitelinks_key else {} for k, v in json_item['sitelinks'].items() if k.endswith('wiki')}
 
-        # Set the separators to avoid spaces
-        output_stream.write(json.dumps(json_item, separators=(',', ':')))
+        # We set the separators to avoid spaces
+        output_line = json.dumps(json_item, separators=(',', ':'))
 
-    output_stream.write('\n]\n')
+        if is_taxon:
+            # Write the line to the output file
+            output_stream.write(output_line)
+            output_stream.write(',\n')
+        else:
+            # If it's vernacular, we'll write it out at the end
+            vernacular_lines.append(output_line)
+
+    # Write out the vernacular lines
+    logging.info(f"Writing {len(vernacular_lines)} vernacular lines at the end of the file")
+    for line in vernacular_lines:
+        output_stream.write(line)
+        output_stream.write(',\n')
+
+    output_stream.write(']\n')
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--verbosity', '-v', action='count', default=0, help='verbosity level: output extra non-essential info')
     parser.add_argument('wikipedia_file', help='The tree file in newick format')
-    parser.add_argument('outputfile', type=argparse.FileType('w'), nargs='?', default=sys.stdout, help='The trimmed output file')
+    parser.add_argument('outputfile', help='The trimmed output file')
     args = parser.parse_args()
 
     if args.verbosity==0:
@@ -103,12 +111,8 @@ def main():
 
     start = time.time()
 
-    if args.wikipedia_file.endswith('.bz2'):
-        line_enumerator = enumerate_lines_from_bz2_file(args.wikipedia_file)
-    else:
-        line_enumerator = enumerate_lines_from_plain_file(args.wikipedia_file)
-
-    process_all_lines(line_enumerator, args.outputfile)
+    with open(args.outputfile, 'wt') as output_stream:
+        process_all_lines(args.wikipedia_file, output_stream)
 
     end = time.time()
     logging.debug("Time taken: {} seconds".format(end - start))

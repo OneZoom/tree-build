ff900d7c19b39ace4ba6dd7266f102645f5952cf
David Ebbo <david.ebbo@gmail.com>
2023-12-30 19:00:26 +0100

    New add_date_species script + bunch of refactoring

--- oz_tree_build/wiki_extraction/wiki_extractor.py
+++ oz_tree_build/wiki_extraction/wiki_extractor.py
@@ -1,35 +1,29 @@
-# https://en.wikipedia.org/wiki/Template:Clade documents the clade template design
+"""
+Extracts a taxon tree from a Wikipedia page.
+
+It supports two types of wikipedia trees.
+
+The first is a taxonomy tree, which is a bullet list of taxa. Here, the location is the name of the header
+that precedes the bullet list. Example command:
+    python3 wiki_extractor.py --title Dinosaur --location 1 Taxonomy
+
+The second is a wikipedia cladogram template. Here, the location is the index of the cladogram within the page.
+Example command:
+    python3 wiki_extractor.py --title Ornithischia --location 1
+
+The output is a Newick tree, which is printed to stdout.
+"""
 
 import argparse
-import mwparserfromhell
-import requests_cache
 import dendropy
-from oz_tree_build.wiki_extraction.mwparserfromhell_helpers import find_wikicode_node
+from oz_tree_build.utilities.debug_util import parse_args_and_add_logging_switch
+from oz_tree_build.wiki_extraction.mwparserfromhell_helpers import (
+    get_wikicode_for_page,
+    get_wikicode_for_string,
+)
 
 from oz_tree_build.wiki_extraction.wiki_clade_node import WikiCladeNode
 from oz_tree_build.wiki_extraction.wiki_taxonomy_node import WikiTaxonomyNode
-
-session = requests_cache.CachedSession("http_cache")
-
-API_URL = "https://en.wikipedia.org/w/api.php"
-
-
-def get_text_from_wiki_page(title):
-    params = {
-        "action": "query",
-        "prop": "revisions",
-        "rvprop": "content",
-        "rvslots": "main",
-        "rvlimit": 1,
-        "titles": title,
-        "format": "json",
-        "formatversion": "2",
-    }
-    headers = {"User-Agent": "My-Bot-Name/1.0"}
-    req = session.get(API_URL, headers=headers, params=params)
-    res = req.json()
-    revision = res["query"]["pages"][0]["revisions"][0]
-    return revision["slots"]["main"]["content"]
 
 
 def process_node(node):
@@ -40,25 +34,22 @@ def process_node(node):
     return tree_node
 
 
-def get_taxon_tree_from_wiki_page_string(wiki_page_string, location):
-    wikicode = mwparserfromhell.parse(wiki_page_string, skip_style_tags=True)
-
+def get_taxon_tree_from_wikicode(wikicode, taxon_tree_location) -> dendropy.Tree:
     # If location string is a number, it's a cladogram index
-    # If it's a string, it's a taxonomy header
-    if location.isnumeric():
-        node = WikiCladeNode.create_root_node(wikicode, int(location))
+    # If it's a string, it's the wiki header that precedes a taxonomy tree (bullet list)
+    if taxon_tree_location.isnumeric():
+        node = WikiCladeNode.create_root_node(wikicode, int(taxon_tree_location))
     else:
-        node = WikiTaxonomyNode.create_root_node(wikicode, location)
+        node = WikiTaxonomyNode.create_root_node(wikicode, taxon_tree_location)
 
     tree = dendropy.Tree()
     tree.seed_node = process_node(node)
-    return tree.as_string(schema="newick")
+    return tree
 
 
-def get_taxon_tree_from_wiki_page(wiki_title, location):
-    wiki_string = get_text_from_wiki_page(wiki_title)
-
-    return get_taxon_tree_from_wiki_page_string(wiki_string, location)
+def get_taxon_tree_from_wiki_page(wiki_title, taxon_tree_location) -> dendropy.Tree:
+    wikicode = get_wikicode_for_page(wiki_title)
+    return get_taxon_tree_from_wikicode(wikicode, taxon_tree_location)
 
 
 def main():
@@ -75,16 +66,19 @@ def main():
         nargs="?",
         help="Index of the cladogram within the page, or name of the taxonomy header",
     )
-    args = parser.parse_args()
+    args = parse_args_and_add_logging_switch(parser)
 
     args.location = args.location or 1
 
     if args.wiki_file:
         with open(args.wiki_file) as f:
             wiki_string = f.read()
-        print(get_taxon_tree_from_wiki_page_string(wiki_string, args.location))
+        wikicode = get_wikicode_for_string(wiki_string)
     else:
-        print(get_taxon_tree_from_wiki_page(args.title, args.location))
+        wikicode = get_wikicode_for_page(args.title)
+
+    tree = get_taxon_tree_from_wikicode(wikicode, args.location)
+    print(tree.as_string(schema="newick"))
 
 
 if __name__ == "__main__":

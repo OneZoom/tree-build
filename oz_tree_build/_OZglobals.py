7ba569a424ae8277b98ebd25653726c179fbc4b7
Yan Wong <yan.wong@bdi.ox.ac.uk>
2024-07-03 19:00:25 +0100

    Remove unused extra funcs

--- oz_tree_build/_OZglobals.py
+++ oz_tree_build/_OZglobals.py
@@ -7,22 +7,13 @@ This is tracked by https://github.com/OneZoom/tree-build/issues/6
 try:
     from gluon import current
 except ImportError:
-    # this is not being used in web2py, but instead in an independent python app
-    # simply define cache.ram to be a function that returns the result of calling the 2nd arg
-    # This is a complex HACK!!!
+    # This is not being used in web2py, but instead in an independent python app.
+    # Simply define cache.ram to be a function that returns the result of calling
+    # the 2nd arg. This is a complex HACK!!!
     cache = type("", (), dict(ram=lambda self, name, func, **kw: func()))()
     current = type(
         "", (), {}
     )()  # allow us to set e.g. current.OZglobals, so we don't bomb out later
-
-    def T(x):
-        """Don't translate when used as an independent app"""
-        return x
-
-    def URL(*args):
-        """Don't make urls when used as an independent app"""
-        return args
-
 
 # bitwise flags for existence of different language wikipedia articles
 # this variable is also used in construct_wiki_info in CSV_base_table_creator.py

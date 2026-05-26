0d3d5e6f70bc525bdcad9729e2d4222014509d81
Jamie Lentin <jm@lentin.co.uk>
2026-05-26 14:49:56 +0000

    node_ages: download_node_ages pipeline stage
    Generate node_ages.json using chronosynth.

---
+++ oz_tree_build/download_node_ages/download_node_ages.py
@@ -0,0 +1,60 @@
+"""
+Populate node_ages.json by calling out to the OpenTree API via. chronosynth
+
+Usage: download_node_ages node_ages.json
+
+NB: The output is not based on the tree downloaded in other steps,
+internally chronosynth will call out to the OpenTree API &
+github.com/OpenTreeOfLife/phylesystem-1.
+
+"""
+
+import argparse
+import json
+import logging
+import os
+import os.path
+import sys
+import time
+
+os.environ["CHRONOSYNTH_CONFIG_FILE"] = os.path.join(os.path.dirname(__file__), "chronosynth_config.ini")
+os.environ["PEYOTL_CONFIG_FILE"] = os.path.join(os.path.dirname(__file__), "peyotl_config.ini")
+
+import chronosynth.chronogram  # noqa: E402 - we need to set env first
+
+
+def download_node_ages():
+    node_ages = chronosynth.chronogram.build_synth_node_source_ages(fresh=True)
+    return node_ages
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__.split("\n\n")[0])
+    parser.add_argument(
+        "--verbosity",
+        "-v",
+        action="count",
+        default=0,
+        help="verbosity level: output extra non-essential info",
+    )
+    parser.add_argument("output_path", help="Path to where output data should be saved")
+    args = parser.parse_args()
+
+    if args.verbosity == 0:
+        logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
+    elif args.verbosity == 1:
+        logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    elif args.verbosity == 2:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    start = time.time()
+
+    with open(args.output_path, "w") as f:
+        json.dump(download_node_ages(), f)
+
+    end = time.time()
+    logging.debug(f"Time taken: {end - start} seconds")
+
+
+if __name__ == "__main__":
+    main()
